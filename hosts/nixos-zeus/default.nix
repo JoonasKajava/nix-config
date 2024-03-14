6decccb9f67fa523c68a2ebfe03139dbd55cea53
Joonas Kajava <JoonasKajava@users.noreply.github.com>
2024-03-15 00:18:08 +0200

    Refactor GNOME and X11 settings into a separate module
    This commit separates the GNOME Desktop Environment and X11 windowing system settings into a new module, gnome.nix. This change provides a more organized configuration setup, making it easier to manage and update in the future.

--- hosts/nixos-zeus/default.nix
+++ hosts/nixos-zeus/default.nix
@@ -7,7 +7,7 @@
 {
   imports = [
    ../../modules/system.nix
-
+   ../../desktops/gnome.nix
    # Include the results of the hardware scan.
    ./hardware-configuration.nix
 
@@ -30,12 +30,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
