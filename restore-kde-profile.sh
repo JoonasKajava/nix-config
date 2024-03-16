496d58a2a0d6f462e992df66e36075f136010fad
Joonas Kajava <JoonasKajava@users.noreply.github.com>
2024-03-17 01:09:49 +0200

    Update path in rsync command in restore-kde-profile.sh
    Adjusted the 'rsync' command in the 'restore-kde-profile.sh' script by adding a slash to the end of the source path. This ensures that the contents of the directory, rather than the directory itself, are synchronized during the KDE profile restoration process.

--- restore-kde-profile.sh
+++ restore-kde-profile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p konsave
 
-rsync -a --delete /etc/nixos/kde-profiles/main ~/.config/konsave/profiles/main
+rsync -a --delete /etc/nixos/kde-profiles/main/ ~/.config/konsave/profiles/main/
 konsave -a main
