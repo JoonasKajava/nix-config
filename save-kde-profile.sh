b57ef49946b8b7e15cb6f3cb484b0e989c61dea1
Joonas Kajava <JoonasKajava@users.noreply.github.com>
2024-03-17 00:54:02 +0200

    Modify konsave command to force save KDE profile
    The code has been updated to modify the konsave command line in the save-kde-profile.bash script to force (with the '-f' flag) the save of the main KDE profile. This change ensures the profile is saved even if it already exists.

--- save-kde-profile.sh
+++ save-kde-profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p konsave
 
-konsave -s main
+konsave -s main -f
 mkdir -p /etc/nixos/kde-profiles/main
 cp -a ~/.config/konsave/profiles/main /etc/nixos/kde-profiles/main
