f2e2c901ef4dde236cd9d56dc6a108d2d38395e7
Joonas Kajava <JoonasKajava@users.noreply.github.com>
2024-03-24 19:24:47 +0200

    modification simplifies the code by removing unnecessary elements and implements the 'config.lib.file.mkOutOfStoreLink' function for managing external files.

--- features/programs/neovim/neovim.nix
+++ features/programs/neovim/neovim.nix
@@ -1,16 +1,13 @@
 { pkgs, lib, user, ... }:
-let
-lazyvim = lib.file.mkOutOfStoreLink "/etc/nixos/features/programs/neovim/lazyvim";
-in
 {
-  home-manager.users.${user.username} = {
+  home-manager.users.${user.username} = {config,...}: {
     programs.neovim = {
       vimAlias = true;
       enable = true;
       viAlias = true;
     };
     xdg.configFile.nvim = {
-      source = lazyvim;
+      source =  config.lib.file.mkOutOfStoreLink "/etc/nixos/features/programs/neovim/lazyvim";
     };
   };
 
