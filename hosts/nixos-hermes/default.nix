430c0be91abaa3fc85e0ca830410a5da776861b4
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-03-25 17:15:32 +0200

    hermes changes

--- hosts/nixos-hermes/default.nix
+++ hosts/nixos-hermes/default.nix
@@ -51,7 +51,6 @@
     polkitPolicyOwners = [ user.username ];
   };
 
-  virtualisation.vmware.guest.enable = true;
 
   environment.systemPackages = with pkgs; [
     firefox
@@ -72,7 +71,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+   services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
