a914f9ff537bc16976b6977bb52015fba1b3a234
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-04-08 14:08:56 +0300

    Added ideavimrc to jetbrains module

--- features/programs/jetbrains/jetbrains.nix
+++ features/programs/jetbrains/jetbrains.nix
@@ -1,6 +1,11 @@
-{ pkgs, user }:
+{ pkgs, lib, user, ... }:
 
 {
+  home-manager.users.${user.username} = { config, ... }: {
+    home.file.".ideavimrc".source = config.lib.file.mkOutOfStoreSymlink
+      "/etc/nixos/features/programs/jetbrains/.ideavimrc";
+  };
+
   environment.systemPackages = with pkgs; [ jetbrains-toolbox ];
 
 }
