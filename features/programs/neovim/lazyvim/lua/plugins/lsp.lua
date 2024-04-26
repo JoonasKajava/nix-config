dbf9c36418cb7150ecb7e59b4c8f09c57b073c57
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-04-27 00:34:26 +0300

    neovim lsp is now configured correctly

--- features/programs/neovim/lazyvim/lua/plugins/lsp.lua
+++ features/programs/neovim/lazyvim/lua/plugins/lsp.lua
@@ -1,28 +1,35 @@
 return {
   {
     {
-      'VonHeikemen/lsp-zero.nvim',
+      "VonHeikemen/lsp-zero.nvim",
+      config = false,
+      lazy = true,
+      init = function()
+        vim.g.lsp_zero_extend_lspconfig = 0
+      end,
+    },
+    {
+      "neovim/nvim-lspconfig",
       config = function()
-        local lsp_zero = require('lsp-zero')
+        local lsp_conf = require("lspconfig")
+        local lsp_zero = require("lsp-zero")
+        lsp_zero.extend_lspconfig()
 
         lsp_zero.on_attach(function(client, bufnr)
           lsp_zero.default_keymaps({ buffer = bufnr })
         end)
-      end
-    },
-    {
-      'neovim/nvim-lspconfig',
-      config = function()
-        local lsp_conf = require('lspconfig')
+
+        local lua_opts = lsp_zero.nvim_lua_ls()
+
+        lsp_conf.lua_ls.setup(lua_opts)
 
         lsp_conf.nil_ls.setup({})
 
-
-        lsp_conf.rust_analyzer.setup{}
-      end
+        lsp_conf.rust_analyzer.setup({})
+      end,
     },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
-  }
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
+    { "L3MON4D3/LuaSnip" },
+  },
 }
