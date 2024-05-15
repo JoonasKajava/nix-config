b17dd5419715c7d65ecbed0eb1e03120ba89f8cc
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-05-15 13:36:38 +0300

    Update and fix system module

--- modules/system.nix
+++ modules/system.nix
@@ -12,76 +12,77 @@ with lib; {
     nix.gc = {enable = mkEnableOption "Automatic recycling";};
   };
 
-  #
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${user.username} = {
-    isNormalUser = true;
-    description = user.name;
-    extraGroups = ["networkmanager" "wheel"];
-  };
+  config = {
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.${user.username} = {
+      isNormalUser = true;
+      description = user.name;
+      extraGroups = ["networkmanager" "wheel"];
+    };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  nix.settings = {experimental-features = ["nix-command flakes"];};
-  nix.settings.auto-optimise-store = true;
+    nix.settings = {experimental-features = ["nix-command flakes"];};
+    nix.settings.auto-optimise-store = true;
 
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    dates = lib.mkDefault "weekly";
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
+    nix.gc = {
+      automatic = lib.mkDefault true;
+      dates = lib.mkDefault "weekly";
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Helsinki";
+    # Set your time zone.
+    time.timeZone = "Europe/Helsinki";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fi_FI.UTF-8";
-    LC_IDENTIFICATION = "fi_FI.UTF-8";
-    LC_MEASUREMENT = "fi_FI.UTF-8";
-    LC_MONETARY = "fi_FI.UTF-8";
-    LC_NAME = "fi_FI.UTF-8";
-    LC_NUMERIC = "fi_FI.UTF-8";
-    LC_PAPER = "fi_FI.UTF-8";
-    LC_TELEPHONE = "fi_FI.UTF-8";
-    LC_TIME = "fi_FI.UTF-8";
-  };
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "fi_FI.UTF-8";
+      LC_IDENTIFICATION = "fi_FI.UTF-8";
+      LC_MEASUREMENT = "fi_FI.UTF-8";
+      LC_MONETARY = "fi_FI.UTF-8";
+      LC_NAME = "fi_FI.UTF-8";
+      LC_NUMERIC = "fi_FI.UTF-8";
+      LC_PAPER = "fi_FI.UTF-8";
+      LC_TELEPHONE = "fi_FI.UTF-8";
+      LC_TIME = "fi_FI.UTF-8";
+    };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    gh
-    git
-    htop-vim
-    tldr
-    difftastic
-    fastfetch
-    rm-improved
-    eza
-  ];
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      gh
+      git
+      htop-vim
+      tldr
+      difftastic
+      fastfetch
+      rm-improved
+      eza
+    ];
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # Enable sound with pipewire.
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
   };
 }
