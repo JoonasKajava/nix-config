778225cb6edb2a5ef81cab762ab5f465843f1536
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-01 00:18:51 +0300

    style(kde): Renames

--- desktops/kde.nix
+++ desktops/kde.nix
@@ -8,7 +8,14 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager = {
+    autoLogin.enable = true;
+    autoLogin.user = "joonas";
+    sddm = {
+      enable = true;
+    };
+  };
+
   services.xserver.desktopManager.plasma5.enable = true;
 
   qt = {
