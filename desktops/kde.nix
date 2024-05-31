d8305fc43fdeb4f9cb8bc2e4d42a7a5a1f41f1af
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-01 00:08:26 +0300

    Revert "feat(kde): Enable plasma 6"
    This reverts commit 94baa445a5b4d6b04a135bb63f475b00020416ea.

--- desktops/kde.nix
+++ desktops/kde.nix
@@ -5,7 +5,11 @@
   user,
   ...
 }: {
-  services.desktopManager.plasma6.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   qt = {
     enable = true;
