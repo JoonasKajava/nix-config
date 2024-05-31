94baa445a5b4d6b04a135bb63f475b00020416ea
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-05-31 23:35:45 +0300

    feat(kde): Enable plasma 6

--- desktops/kde.nix
+++ desktops/kde.nix
@@ -5,11 +5,7 @@
   user,
   ...
 }: {
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   qt = {
     enable = true;
