d1aa09f5e89f3831a303017670d582eab836020d
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-01 01:51:33 +0300

    feat(system): Add gnome keyring

--- hosts/nixos-desktop/default.nix
+++ hosts/nixos-desktop/default.nix
@@ -58,6 +58,8 @@
     nativeMessagingHosts.packages = [pkgs.firefoxpwa];
   };
 
+  services.gnome.gnome-keyring.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
