0935825e33a6d6f8506831ba8157bce5fc13e5a7
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-02 03:02:45 +0300

    feat(neovim): Added clipboard functionality

--- features/programs/neovim/neovim.nix
+++ features/programs/neovim/neovim.nix
@@ -1,12 +1,18 @@
-{ pkgs, lib, user, ... }: {
-  home-manager.users.${user.username} = { config, ... }: {
+{
+  pkgs,
+  lib,
+  user,
+  ...
+}: {
+  home-manager.users.${user.username} = {config, ...}: {
     programs.neovim = {
       vimAlias = true;
       enable = true;
       viAlias = true;
     };
     xdg.configFile.nvim = {
-      source = config.lib.file.mkOutOfStoreSymlink
+      source =
+        config.lib.file.mkOutOfStoreSymlink
         "/etc/nixos/features/programs/neovim/lazyvim";
     };
   };
@@ -17,10 +23,9 @@
   };
 
   nix.settings = {
-    trusted-public-keys =
-      [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
-    substituters = [ "https://devenv.cachix.org" ];
-    trusted-users = [ "root" user.username ];
+    trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
+    substituters = ["https://devenv.cachix.org"];
+    trusted-users = ["root" user.username];
   };
 
   programs.direnv.enable = true;
@@ -32,11 +37,10 @@
       ripgrep
       lazygit
       cargo
-
+      xclip
       devenv
-
     ];
   };
 
-  fonts.packages = with pkgs; [ nerdfonts ];
+  fonts.packages = with pkgs; [nerdfonts];
 }
