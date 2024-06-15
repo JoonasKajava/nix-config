ec6ec980639637460ce3498297d67090e3d4c570
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-15 23:20:35 +0300

    feat(neovim): added github search to neovim

---
+++ features/programs/neovim/lazyvim/lua/plugins/github.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "napisani/nvim-github-codesearch",
+    build = "make",
+    config = function()
+      local gh_search = require("nvim-github-codesearch")
+      gh_search.setup({
+        use_telescope = true,
+      })
+      vim.keymap.set("n", "<leader>fs", "<cmd>lua require('nvim-github-codesearch').prompt()<CR>", {
+        desc = "Find From Github",
+      })
+    end,
+  },
+}
