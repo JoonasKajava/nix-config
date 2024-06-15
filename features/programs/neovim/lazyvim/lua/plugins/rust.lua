25f85632b9f6998fc2a383429d05231e7ffde721
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-15 18:48:37 +0300

    feat(rust): added neovim crates keybindings

--- features/programs/neovim/lazyvim/lua/plugins/rust.lua
+++ features/programs/neovim/lazyvim/lua/plugins/rust.lua
@@ -7,10 +7,20 @@ return {
     "saecki/crates.nvim",
     event = { "BufRead Cargo.toml" },
     config = function()
-      require("crates").setup({
+      local crates = require("crates")
+      crates.setup({
+        popup = { autofocus = true },
         null_ls = {
           enabled = true,
         },
+      })
+
+      vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, {
+        desc = "Show Versions",
+      })
+
+      vim.keymap.set("n", "<leader>cf", crates.show_features_popup, {
+        desc = "Show Features",
       })
     end,
   },
