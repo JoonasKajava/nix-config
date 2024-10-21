841a4decb16d48ef61e264435e00a7b1760bf4e6
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-10-21 18:05:03 +0300

    feat: added dotnet as system dependency

--- features/programs/jetbrains/jetbrains.nix
+++ features/programs/jetbrains/jetbrains.nix
@@ -10,5 +10,12 @@
       "/etc/nixos/features/programs/jetbrains/.ideavimrc";
   };
 
-  environment.systemPackages = with pkgs.jetbrains; [rider];
+  environment.systemPackages = with pkgs; [
+    jetbrains.rider
+    dotnet-sdk_8
+  ];
+
+  environment.sessionVariables = {
+    DOTNET_ROOT = "${pkgs.dotnet-sdk}";
+  };
 }
