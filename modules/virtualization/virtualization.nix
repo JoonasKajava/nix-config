f15aa3fb6ef079683a5ca149a4c51eb062fafca0
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-10-28 16:38:03 +0200

    feat: added virtual machine stuff for windows 11

--- modules/virtualization/virtualization.nix
+++ modules/virtualization/virtualization.nix
@@ -1,6 +1,8 @@
 {
   lib,
   config,
+  user,
+  pkgs,
   ...
 }:
 with lib; let
@@ -9,7 +11,32 @@ in {
   options.mystuff.virtualization.enable = mkEnableOption "Virtualization";
 
   config = mkIf cfg.enable {
-    virtualisation.libvirtd.enable = true;
+    users.users.${user.username}.extraGroups = ["libvirtd"];
+
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        qemu = {
+          swtpm.enable = true;
+          ovmf.enable = true;
+          ovmf.packages = [pkgs.OVMFFull.fd];
+        };
+      };
+      spiceUSBRedirection.enable = true;
+    };
+
+    services.spice-vdagentd.enable = true;
+
     programs.virt-manager.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      virt-viewer
+      spice
+      spice-gtk
+      spice-protocol
+      win-virtio
+      win-spice
+      # gnome.adwaita-icon-theme
+    ];
   };
 }
