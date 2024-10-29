50861f0742bc99c64bf8f020e50214f7fa70655d
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-10-29 12:08:17 +0200

    feat: added wrapper

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -14,5 +14,20 @@ in {
     programs.yazi = {
       enable = true;
     };
+
+    home-manager.users.${user.username} = {
+      programs.zsh = {
+        initExtra = ''
+          function y() {
+          	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+          	yazi "$@" --cwd-file="$tmp"
+          	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          		builtin cd -- "$cwd"
+          	fi
+          	rm -f -- "$tmp"
+          }
+        '';
+      };
+    };
   };
 }
