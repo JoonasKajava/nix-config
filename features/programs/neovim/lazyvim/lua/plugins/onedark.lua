358f96d19c931d5db027d3ecc14b8adb9357358e
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-11-08 23:42:50 +0200

    feat: theming

---
+++ features/programs/neovim/lazyvim/lua/plugins/onedark.lua
@@ -0,0 +1,22 @@
+if true then
+  return {}
+end
+return {
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      require("onedark").load()
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
