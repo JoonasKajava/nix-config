18c028fd3cd1da41bfa3f2f0efd9ef36a22d75b2
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-11-28 11:46:24 +0200

    feat: added freerdp

---
+++ modules/networking/remote/freerdp.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.mystuff.networking.remote.freerdp;
+in {
+  options.mystuff.networking.remote.freerdp = {
+    enable = mkEnableOption "freeRDP";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      freerdp3
+    ];
+  };
+}
