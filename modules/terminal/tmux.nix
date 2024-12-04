5d24cc1d54477e7e7244efa575084e9c073575f9
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-12-04 21:19:34 +0200

    feat: tmux colors

--- modules/terminal/tmux.nix
+++ modules/terminal/tmux.nix
@@ -16,6 +16,10 @@ in {
       enable = true;
       extraConfig = ''
         set-option -g mouse on
+        set -g default-terminal "tmux-256color"
+        set -ga terminal-overrides ",*256col*:Tc"
+        set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+        set-environment -g COLORTERM "truecolor"
       '';
     };
   };
