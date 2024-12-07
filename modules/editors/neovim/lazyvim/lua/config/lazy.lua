766f2af94a1f23ffa40a27f3ae7a7c7bfceb2d20
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-12-07 17:57:11 +0200

    feat: add mini surround

--- modules/editors/neovim/lazyvim/lua/config/lazy.lua
+++ modules/editors/neovim/lazyvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
