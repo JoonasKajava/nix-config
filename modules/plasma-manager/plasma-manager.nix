5a7cf6ea8cd5321ddda7115e8804a99b30bdf4fb
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-12-28 01:32:32 +0200

    feat: progress on plasma manager

--- modules/plasma-manager/plasma-manager.nix
+++ modules/plasma-manager/plasma-manager.nix
@@ -2,16 +2,107 @@
   config,
   lib,
   pkgs,
+  user,
+  home-manager,
   ...
 }:
 with lib; let
   cfg = config.mystuff.plasma-manager;
+  mkPwaDesktopItem = {
+    name,
+    app,
+    icon,
+  }:
+    pkgs.makeDesktopItem {
+      name = "${name}-pwa-desktop";
+      desktopName = name;
+      exec = "${pkgs.brave}/bin/brave --app\"${app}\"";
+    };
 in {
   options.mystuff.plasma-manager = {
     enable = mkEnableOption "plasma manager";
   };
 
-  config =
-    mkIf cfg.enable {
+  config = mkIf cfg.enable {
+    home-manager.users.${user.username} = {
+      programs.plasma = {
+        enable = true;
+
+        hotkeys.commands."1password-quick-access" = mkIf config.mystuff.onepassword.enable {
+          name = "Open Quick Access";
+          key = "Ctrl+Alt+Shift+P";
+          command = "1password --quick-access";
+        };
+
+        panels = [
+          {
+            location = "bottom";
+            screen = "all";
+            widgets = [
+              {
+                kickoff = {
+                  icon = "nix-snowflake-white";
+                };
+              }
+              {
+                iconTasks = {
+                  appearance = {
+                    indicateAudioStreams = false;
+                  };
+                  launchers =
+                    [
+                      "applications:systemsettings.desktop"
+                      "preferred://filemanager"
+                    ]
+                    ++ (lib.optionals config.mystuff.brave.enable [
+                      "${pkgs.brave}/share/applications/brave-browser.desktop"
+                      mkPwaDesktopItem
+                      {
+                        name = "Whatsapp";
+                        app = "https://web.whatsapp.com/";
+                        icon = builtins.fetchurl "https://static.whatsapp.net/rsrc.php/v4/yw/r/WDR7jNjkVfM.png";
+                      }
+                      mkPwaDesktopItem
+                      {
+                        name = "FastMail";
+                        app = "https://app.fastmail.com/";
+                        icon = builtins.fetchurl "https://app.fastmail.com/static/appicons/Icon-MacOS-512x512@2x.png";
+                      }
+                    ])
+                    ++ (lib.optionals config.mystuff.office.obsidian.enable [
+                      "${pkgs.obsidian}/share/applications/obsidian.desktop"
+                    ])
+                    ++ (lib.optionals config.mystuff.gaming.steam.enable [
+                      "${pkgs.steam}/share/applications/steam.desktop"
+                    ])
+                    ++ (lib.optionals config.mystuff.onepassword.enable [
+                      "${pkgs._1password-gui}/share/applications/1password.desktop"
+                    ])
+                    ++ (lib.optionals config.mystuff.discord.enable [
+                      "${pkgs.discord-canary}/share/applications/discord-canary.desktop"
+                    ]);
+                };
+              }
+              {
+                name = "org.kde.plasma.systemmonitor.cpucore";
+              }
+              "org.kde.plasma.systemtray"
+              {
+                digitalClock = {
+                  time = {
+                    format = "24h";
+                    showSeconds = "always";
+                  };
+                  calendar = {
+                    firstDayOfWeek = "monday";
+                    showWeekNumbers = true;
+                  };
+                };
+              }
+            ];
+          }
+        ];
+      };
     };
+  };
 }
