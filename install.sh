80eb7af12c7c191a7937c070883b161f57484c98
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-02-18 14:35:24 +0200

    feat: proper install script draft, not tested yet

---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p gh
+
+gh auth login
+
+gh repo clone JoonasKajava/nix-config /etc/nixos/
+
+cd /etc/nixos/
+
+git submodule update --init --recursive
+
+echo "Enter which configuration you want to use:"
+read hostname
+
+echo "Enter the system architecture for this host (eg. \'x86_64-linux\')"
+read system
+
+nixos-generate-config --show-hardware-config >/etc/nixos/systems/${system}/${hostname}/hardware.nix
+
+rm /etc/nixos/configuration.nix
+rm /etc/nixos/hardware-configuration.nix
+
+export NIXPKGS_ALLOW_UNFREE=1
+
+nixos-rebuild switch --flake .#${hostname} --experimental-features 'nix-command flakes'
+
+gh auth logout
+
+echo "Install configuration for ${hostname} complete. Remember to execute setup scripts from private flake."
