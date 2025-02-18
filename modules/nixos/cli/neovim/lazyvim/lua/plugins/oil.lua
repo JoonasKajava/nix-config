a9749b034f6c8bf8c664c8fd0f6293e5509be234
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-02-18 15:16:47 +0200

    feat: oil.nvim now shows hidden files by default

--- modules/nixos/cli/neovim/lazyvim/lua/plugins/oil.lua
+++ modules/nixos/cli/neovim/lazyvim/lua/plugins/oil.lua
@@ -3,7 +3,12 @@ return {
     "stevearc/oil.nvim",
     config = function()
       local oil_conf = require("oil")
-      oil_conf.setup()
+      oil_conf.setup({
+        default_file_explorer = true,
+        view_options = {
+          show_hidden = true,
+        },
+      })
 
       vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>")
     end,
