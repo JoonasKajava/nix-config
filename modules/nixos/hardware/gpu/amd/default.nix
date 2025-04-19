927cfd8bec22b8d06164a8672ed76639faf48c9c
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-04-19 20:55:59 +0300

    feat: add few things to try reduce crashing

--- modules/nixos/hardware/gpu/amd/default.nix
+++ modules/nixos/hardware/gpu/amd/default.nix
@@ -16,8 +16,13 @@ in {
   config = mkIf cfg.enable {
     boot.kernelParams = [
       "amdgpu.gpu_recovery=1"
+      "amdgpu.noretry=0"
       "panic=30"
     ];
+
+    environment.variables = {
+      KWIN_DRM_NO_DIRECT_SCANOUT = "1"; # TODO ONLY TO TEST GPU GRASH
+    };
 
     hardware.graphics = {
       enable = true;
