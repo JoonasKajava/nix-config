7c103e208d9d8cb79ca51fcb8a092ac32573eb6f
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-05-02 16:50:55 +0300

    fix: fixes to hyprpanel

--- modules/home/desktop/hyprland/hyprpanel.nix
+++ modules/home/desktop/hyprland/hyprpanel.nix
@@ -9,31 +9,31 @@
 
   programs.hyprpanel = lib.mkIf config.${namespace}.desktop.hyprland.enable {
     enable = true;
-    systemd.enable = true;
     hyprland.enable = true;
     override.enable = true;
 
+    #TODO: do i need overlay.enable = true?
+
     theme = "catppuccin_mocha";
 
-    layout = {
-      "bar.layouts" = {
-        "*" = {
-          left = ["dashboard" "workspaces" "windowtitle"];
-          middle = ["weather" "clock" "notifications"];
-          right = [
-            "systray"
-            "kbinput"
-            "volume"
-            "microphone"
-            "network"
-            "bluetooth"
-            "power"
-          ];
+    settings = {
+      layout = {
+        "bar.layouts" = {
+          "*" = {
+            left = ["dashboard" "workspaces" "windowtitle"];
+            middle = ["weather" "clock" "notifications"];
+            right = [
+              "systray"
+              "kbinput"
+              "volume"
+              "microphone"
+              "network"
+              "bluetooth"
+              "power"
+            ];
+          };
         };
       };
-    };
-
-    settings = {
       bar = {
         clock = {
           format = "%a %b %d  %H:%M:%S";
