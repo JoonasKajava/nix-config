076022c84f5491700677094854a728af07dc8b5b
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-06-04 20:28:13 +0300

    fix: small fixes

--- modules/nixos/services/docker/containers/wallos.nix
+++ modules/nixos/services/docker/containers/wallos.nix
@@ -3,8 +3,8 @@
   ports = ["8282:80/tcp"];
   # TODO: Move these volumes to better place
   volumes = [
-    "/home/${config.user.name}/.wallos/db:/var/www/html/db:rw"
-    "/home/${config.user.name}/.wallos/logos:/var/www/html/images/uploads/logos:rw"
+    "/var/www/wallos/db:/var/www/html/db:rw"
+    "/var/www/wallos/logos:/var/www/html/images/uploads/logos:rw"
   ];
   environment = {
     TZ = "Europe/Berlin";
