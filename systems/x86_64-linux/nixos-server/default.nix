b08acd6e37ef0c748e9c75307992a106c3d3b98e
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-06-04 18:04:44 +0300

    feat: add nixos-server configuration

---
+++ systems/x86_64-linux/nixos-server/default.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; {
+  imports = [./hardware.nix];
+  boot = {
+    loader = {
+      # Bootloader.
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  lumi = {
+    suites.cli.enable = true;
+
+    services = {
+      docker = {
+        enable = true;
+        wallos = true;
+      };
+    };
+  };
+
+  lumi-private = {
+    services.borgbackup.enable = true;
+    services.ssh.enable = true;
+  };
+
+  networking.hostName = "nixos-server"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
