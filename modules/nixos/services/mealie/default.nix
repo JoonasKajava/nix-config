09feb74bb583d50660de836503249df34a18d201
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-06-17 15:44:00 +0300

    feat: add mealie service

---
+++ modules/nixos/services/mealie/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  config,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf mkOption;
+
+  cfg = config.${namespace}.services.mealie;
+in {
+  options.${namespace}.services.mealie = {
+    enable = mkEnableOption "Whether to enable mealie";
+    host = mkOption {
+      type = lib.types.str;
+      default = "mealie.joonaskajava.com";
+      description = "The host name for the mealie service";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    registery.importantDirs = [
+      "/var/lib/mealie/"
+    ];
+
+    services.mealie = {
+      enable = true;
+      settings = {
+        BASE_URL = "https://${cfg.host}";
+        TZ = "Europe/Helsinki";
+        ALLOW_SIGNUP = false;
+      };
+    };
+
+    sops.templates."mealie-env".content = ''
+      SMTP_HOST=${config.sops.placeholder."smtp/host"}
+      SMTP_PORT=${config.sops.placeholder."smtp/port-starttls"}
+      SMTP_USER=${config.sops.placeholder."smtp/username"}
+      SMTP_PASSWORD=${config.sops.placeholder."smtp/app-password"}
+      SMTP_FROM_NAME=Mealie
+      SMTP_FROM_EMAIL=mealie@${cfg.host}
+
+    '';
+    systemd.services.mealie = {
+      after = ["sops-nix.service"];
+      serviceConfig.EnvironmentFile = [
+        config.sops.templates."mealie-env".path
+      ];
+    };
+
+    services.caddy.virtualHosts."${cfg.host}" = {
+      extraConfig = ''
+        reverse_proxy 127.0.0.1:${toString cfg.internalPort}
+      '';
+    };
+  };
+}
