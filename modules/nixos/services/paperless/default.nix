320768e10cf696465c53801bc48bf48061712b35
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-06-21 22:06:42 +0300

    feat: paperless ai now uses podman

--- modules/nixos/services/paperless/default.nix
+++ modules/nixos/services/paperless/default.nix
@@ -42,7 +42,7 @@ in {
       docker.enable = true;
 
       oci-containers = {
-        backend = "docker";
+        backend = "podman";
 
         containers = {
           paperless-ai = {
@@ -52,7 +52,7 @@ in {
               "/var/lib/paperless/paperless-ai:/app/data:rw"
             ];
             autoStart = true;
-            extraOptions = ["--pull=always" "--network=host"];
+            extraOptions = ["--pull=newer" "--network=host"];
           };
         };
       };
