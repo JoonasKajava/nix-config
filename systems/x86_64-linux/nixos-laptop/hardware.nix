6e14bf08b8f931095b6ef20fb3574b5a3b5c181b
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-07-05 17:09:02 +0300

    feat: try fix laptop audio

--- systems/x86_64-linux/nixos-laptop/hardware.nix
+++ systems/x86_64-linux/nixos-laptop/hardware.nix
@@ -36,4 +36,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableAllFirmware = true;
+  hardware.enableAllHardware = true;
+  hardware.enableRedistributableFirmware = true;
 }
