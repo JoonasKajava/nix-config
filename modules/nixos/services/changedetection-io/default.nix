5d95eaee7d2d88e9da88c81f9ccc4659c6d892da
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-12-04 18:46:48 +0200

    feat: fix changedetection

--- modules/nixos/services/changedetection-io/default.nix
+++ modules/nixos/services/changedetection-io/default.nix
@@ -5,9 +5,10 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf mkOption;
+  inherit (lib) mkEnableOption mkIf mkOption types;
 
   cfg = config.${namespace}.services.changedetection-io;
+  dataDir = "/var/lib/changedetection-io";
 in {
   options.${namespace}.services.changedetection-io = {
     enable = mkEnableOption "Whether to enable changedetection-io";
@@ -16,33 +17,55 @@ in {
       default = "changedetection.joonaskajava.com";
       description = "The host name for the changedetection-io service";
     };
+
+    port = mkOption {
+      type = types.number;
+      default = 33253;
+    };
+
+    playwright-port = mkOption {
+      type = types.number;
+      default = 33937;
+    };
   };
 
   config = mkIf cfg.enable {
     registery.importantDirs = [
-      config.services.changedetection-io.datastorePath
+      dataDir
     ];
+    virtualisation.oci-containers.containers = {
+      changedetection-io = {
+        image = "dgtlmoon/changedetection.io:latest";
+        ports = ["${builtins.toString cfg.port}:5000/tcp"];
+        volumes = [
+          "${dataDir}:/datastore:rw"
+        ];
+        autoStart = true;
+        extraOptions = ["--pull=newer"];
+        environment = {
+          PLAYWRIGHT_DRIVER_URL = "ws://host.containers.internal:${builtins.toString cfg.playwright-port}";
+          BASE_URL = "https://${cfg.host}";
+        };
+      };
 
-    services.changedetection-io = {
-      enable = true;
-      behindProxy = true;
-      baseURL = "https://${cfg.host}";
-      playwrightSupport = true;
+      changedetection-io-playwright = {
+        image = "dgtlmoon/sockpuppetbrowser:latest";
+        ports = ["${builtins.toString cfg.playwright-port}:3000/tcp"];
+        autoStart = true;
+        extraOptions = ["--pull=newer"];
+        environment = {
+          SCREEN_WIDTH = "1920";
+          SCREEN_HEIGHT = "1024";
+          SCREEN_DEPTH = "16";
+          MAX_CONCURRENT_CHROME_PROCESSES = "10";
+          CHROME_OPTIONS = "--window-size=1280,1024 --headless --disable-gpu";
+        };
+      };
     };
-    virtualisation.oci-containers.containers.changedetection-io-playwright = {
-      extraOptions = ["--pull=newer"];
-      #image = lib.mkForce "dgtlmoon/sockpuppetbrowser:latest";
-    };
-
-    environment.systemPackages = with pkgs; [
-      playwright
-      python313Packages.playwright
-    ];
-
     services = {
       caddy.virtualHosts."${cfg.host}" = {
         extraConfig = ''
-          reverse_proxy localhost:${toString config.services.changedetection-io.port}
+          reverse_proxy localhost:${toString cfg.port}
           import cloudflare
         '';
       };
