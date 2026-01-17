db7235025e5d0249f8a4e44987a9209aa34deb5c
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-01-17 14:46:33 +0200

    feat: enable proton env variables

--- modules/nixos/apps/steam/default.nix
+++ modules/nixos/apps/steam/default.nix
@@ -11,6 +11,12 @@ in {
   options.${namespace}.apps.steam = {enable = mkEnableOption "steam";};
 
   config = mkIf cfg.enable {
+    environment.sessionVariables = {
+      PROTON_FSR4_UPGRADE = "1";
+      PROTON_XESS_UPGRADE = "1";
+      PROTON_ENABLE_WAYLAND = "1";
+    };
+
     programs = {
       steam = {
         enable = true;
