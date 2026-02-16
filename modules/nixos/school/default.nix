6a8fc280c3c0b346883673c9fb3376707e40b4d6
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-02-16 18:32:11 +0200

    fix: fix pycharm

--- modules/nixos/school/default.nix
+++ modules/nixos/school/default.nix
@@ -7,6 +7,19 @@
 }:
 with lib; let
   cfg = config.${namespace}.school;
+  pythonldlibpath = lib.makeLibraryPath (with pkgs; [
+    zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
+  ]);
+  # Darwin requires a different library path prefix
+  wrapPrefix = if (!pkgs.stdenv.isDarwin) then "LD_LIBRARY_PATH" else "DYLD_LIBRARY_PATH";
+  patchedpython = (pkgs.symlinkJoin {
+    name = "python";
+    paths = [ pkgs.python312 ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram "$out/bin/python3.12" --prefix ${wrapPrefix} : "${pythonldlibpath}"
+    '';
+  });
 in {
   options.${namespace}.school = {enable = mkEnableOption "school";};
 
@@ -14,12 +27,15 @@ in {
     environment.systemPackages = with pkgs; [
       zoom-us
       vscode
-      (python3.withPackages (p: with p; [
-        pytest
-        numpy
-        pandas
-        matplotlib
-      ]))
+      patchedpython
+      # (python3.withPackages (p:
+      #   with p; [
+      #     pytest
+      #     numpy
+      #     pandas
+      #     matplotlib
+      #     jupyter
+      #   ]))
     ];
 
     lumi.apps.jetbrains.ide.pycharm = true;
