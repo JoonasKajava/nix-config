509f6dcb5952c749136dc8fcbf8b0a6083c3bc2b
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-06-30 16:11:43 +0300

    feat: system upgrade

--- modules/nixos/services/caddy/default.nix
+++ modules/nixos/services/caddy/default.nix
@@ -16,7 +16,7 @@ in {
     services.caddy = {
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-        hash = "sha256-wHW0l15aLswe7gV9WioXo//abd0sJI82I7zIroRG3uU=";
+        hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
       };
       extraConfig = mkIf enableCloudflareIntegration ''
         (cloudflare) {
