ff9fa2418a0e8ec77ff923ee3ef230cc0561d6d6
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-08-18 00:23:05 +0300

    feat: small updates

--- modules/nixos/cli/nushell/nushell.nix
+++ modules/nixos/cli/nushell/nushell.nix
@@ -1,67 +1,70 @@
-  {
-    den.aspects.cli.nushell = {
-      nixos = {pkgs, ...}: {
-        users.defaultUserShell = pkgs.nushell;
-      };
-      homeManager = {pkgs, lib, config, ...}: {
-        home.shell.enableNushellIntegration = true;
+{
+  den.aspects.cli.nushell = {
+    nixos = {pkgs, ...}: {
+      users.defaultUserShell = pkgs.nushell;
+    };
+    homeManager = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      home.shell.enableNushellIntegration = true;
 
-        programs = {
-          carapace.enable = true;
+      programs = {
+        carapace.enable = true;
 
-          fish = {
-            enable = true;
-            generateCompletions = true;
+        fish = {
+          enable = true;
+          generateCompletions = true;
+        };
+
+        nix-your-shell.enable = true;
+
+        nushell = let
+          plugins = with pkgs.nushellPlugins; [query];
+        in {
+          enable = true;
+          configFile.source = ./config.nu;
+          environmentVariables = {
+            CARAPACE_BRIDGES = "fish";
           };
+          settings = {
+            keybindings = [
+              {
+                modifier = "control";
+                keycode = "char_l";
+                mode = ["emacs" "vi_normal" "vi_insert"];
+                event = null;
+              }
+            ];
+          };
+          extraConfig =
+            # nushell
+            ''
+              ${lib.getExe pkgs.fastfetch}
 
-          nix-your-shell.enable = true;
-
-          nushell = 
-          let 
-            plugins = [];
-            in
-            {
-            enable = true;
-            configFile.source = ./config.nu;
-            environmentVariables = {
-              CARAPACE_BRIDGES = "fish";
-            };
-            settings = {
-              keybindings = [
-                {
-                  modifier = "control";
-                  keycode = "char_l";
-                  mode = ["emacs" "vi_normal" "vi_insert"];
-                  event = null;
+              let fish_completer = {|spans|
+                ${lib.getExe config.programs.fish.package} --command $"complete '--do-complete=($spans | str join ' ')'"
+                | from tsv --flexible --noheaders --no-infer
+                | rename value description
+                | update value {
+                    if ($in | path exists) {$'"($in | str replace "\"" "\\\"" )"'} else {$in}
                 }
-              ];
-            };
-            extraConfig =
-              # nushell
-              ''
-                ${ lib.getExe pkgs.fastfetch }
+              }
 
-                let fish_completer = {|spans|
-                  ${lib.getExe config.programs.fish.package} --command $"complete '--do-complete=($spans | str join ' ')'"
-                  | from tsv --flexible --noheaders --no-infer
-                  | rename value description
-                  | update value {
-                      if ($in | path exists) {$'"($in | str replace "\"" "\\\"" )"'} else {$in}
+              $env.config = {
+                completions: {
+                  external: {
+                    enable: true
+                    completer: $fish_completer
                   }
                 }
-
-                $env.config = {
-                  completions: {
-                    external: {
-                      enable: true
-                      completer: $fish_completer
-                    }
-                  }
-                }
-              ''
-              + lib.concatStringsSep "\n" (map (p: "plugin add ${lib.getExe p}") plugins);
-          };
+              }
+            ''
+            + lib.concatStringsSep "\n" (map (p: "plugin add ${lib.getExe p}") plugins);
         };
       };
     };
-  }
+  };
+}
