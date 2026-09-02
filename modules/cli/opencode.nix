51164f3ebc1cf3805a6d5044632869103e549bf5
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-09-02 18:45:25 +0300

    feat: try add brave search

--- modules/cli/opencode.nix
+++ modules/cli/opencode.nix
@@ -1,7 +1,13 @@
 {
+  den.aspects.opencode.nixos = {
+    sops.secrets."brave-search-api-key" = {
+      owner = "joonas";
+    };
+  };
   den.aspects.opencode.homeManager = {
     pkgs,
     lib,
+    osConfig,
     ...
   }: {
     programs.mcp = {
@@ -15,6 +21,13 @@
           type = "local";
           command = lib.getExe pkgs.context7-mcp;
           enabled = true;
+        };
+        brave-search = {
+          command = "${pkgs.nodejs}/bin/npx";
+          args = ["-y" "@brave/brave-search-mcp-server" "--transport" "http"];
+          env = {
+            BRAVE_API_KEY.file = osConfig.sops.secrets."brave-search-api-key".path;
+          };
         };
       };
     };
