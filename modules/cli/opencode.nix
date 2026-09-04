1c8cea75a449c5be0349cf0a091020b92bba1b2c
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-09-04 11:44:17 +0300

    feat: opencode changes

--- modules/cli/opencode.nix
+++ modules/cli/opencode.nix
@@ -1,39 +1,45 @@
-{
-  den.aspects.opencode.nixos = {
-    sops.secrets."brave-search-api-key" = {
-      owner = "joonas";
+{den,...}:{
+  den.aspects.opencode = {
+    includes = with den.aspects; [
+      sops
+      skills
+    ];
+    nixos = {
+      sops.secrets."brave-search-api-key" = {
+        owner = "joonas";
+      };
     };
-  };
-  den.aspects.opencode.homeManager = {
-    pkgs,
-    lib,
-    osConfig,
-    ...
-  }: {
-    programs.mcp = {
-      enable = true;
-      servers = {
-        nixos = {
-          command = lib.getExe pkgs.mcp-nixos;
-          enabled = true;
-        };
-        context7 = {
-          type = "local";
-          command = lib.getExe pkgs.context7-mcp;
-          enabled = true;
-        };
-        brave-search = {
-          command = "${pkgs.nodejs}/bin/npx";
-          args = ["-y" "@brave/brave-search-mcp-server"];
-          env = {
-            BRAVE_API_KEY_FILE = osConfig.sops.secrets."brave-search-api-key".path;
+    homeManager = {
+      pkgs,
+      lib,
+      osConfig,
+      ...
+    }: {
+      programs.mcp = {
+        enable = true;
+        servers = {
+          nixos = {
+            command = lib.getExe pkgs.mcp-nixos;
+            enabled = true;
+          };
+          context7 = {
+            type = "local";
+            command = lib.getExe pkgs.context7-mcp;
+            enabled = true;
+          };
+          brave-search = {
+            command = "${pkgs.nodejs}/bin/npx";
+            args = ["-y" "@brave/brave-search-mcp-server"];
+            env = {
+              BRAVE_API_KEY_FILE = osConfig.sops.secrets."brave-search-api-key".path;
+            };
           };
         };
       };
-    };
-    programs.opencode = {
-      enableMcpIntegration = true;
-      enable = true;
+      programs.opencode = {
+        enableMcpIntegration = true;
+        enable = true;
+      };
     };
   };
 }
